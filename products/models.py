e2b91c5189e89ea6034f7bd3650ad27166bbf606
sanydge <sanydge@gmail.com>
2020-09-21 19:51:43 +0300

    search by related model

--- products/models.py
+++ products/models.py
@@ -36,7 +36,9 @@ class ProductQuerySet(models.query.QuerySet):
 
         lookups = (Q(title__icontains=query) |
                    Q(description__icontains=query) |
-                   Q(price__icontains=query))
+                   Q(price__icontains=query) |
+                   Q(tag__title__icontains=query)
+                   )
         # tag__name__icontains=query
         return self.filter(lookups).distinct()
 
