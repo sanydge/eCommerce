d111607c060867d758b1d254964f1819df92da4f
sanydge <sanydge@gmail.com>
2020-09-22 20:23:12 +0300

    M2M changed signal to calculate cart total

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from products.models import Product
 
@@ -21,12 +22,12 @@ class CartManager(models.Manager):
             cart_obj = Cart.objects.new(user=request.user)
             new_obj = True
             request.session['cart_id'] = cart_obj.id
-        return cart_obj
+        return cart_obj, new_obj
 
     def new(self, user=None):
         user_obj = None
         if user is not None:
-            if user.is_authenticated():
+            if user.is_authenticated:
                 user_obj = user
         return self.model.objects.create(user=user_obj)
 
@@ -34,6 +35,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -42,3 +44,23 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'port_clear':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total == instance.subtotal
+
+    pre_save.connect(pre_save_cart_receiver, sender=Cart)
