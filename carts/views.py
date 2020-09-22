1558117774b3153d8b1f1b347eacdeb61792c885
sanydge <sanydge@gmail.com>
2020-09-22 14:31:55 +0300

    Cart model

--- carts/views.py
+++ carts/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:  #and isinstance(cart_id, int):
+        print('create new cart')
+        request.session['cart_is'] = 12
+    else:
+        print('Cart ID exists')
     return render(request, "carts/home.html", {})
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -6,6 +6,7 @@ from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
+    # print(request.session.get("first_name", "Unknown"))
     context = {
         "title": "Hello World!",
         "content": "Welcome to the homepage.",
